29a5b7ed13f4bfe37f7ccaf28b278c211feaa38d
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-12 14:12:05 -0400

    added model class car

---
+++ app/src/main/java/com/test/carrental/Car.java
@@ -0,0 +1,39 @@
+package com.test.carrental;
+
+import java.io.Serializable;
+
+public class Car implements Serializable {
+    private String name;
+    private double dailyRate;
+    private boolean status;
+
+    public Car(String name, double dailyRate, boolean status) {
+        this.name = name;
+        this.dailyRate = dailyRate;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDailyRate() {
+        return dailyRate;
+    }
+
+    public void setDailyRate(double dailyRate) {
+        this.dailyRate = dailyRate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
